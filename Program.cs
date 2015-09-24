45982f65a947029c3a8902e2137a81d0ca54c2dd
Eugeniya Voroshilo <j_v.k@mail.ru>
2015-09-24 12:21:51 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace line
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inFile = "in.txt";
+            string outFile = "out.txt";
+
+            string[] s = new string[intReadFile(inFile)+1];
+            ReadFile(inFile, s);
+            
+            int N = int.Parse(s[0]);
+            WriteFile(outFile, s[1]);
+        }
+
+        static void WriteFile(string filename, string str)
+        {
+            using (StreamWriter writer = File.CreateText(filename))
+            {
+                string[] nStr = str.Split(' ');
+                for(int i = 0; i < nStr.Length; i++)
+                {
+                    writer.WriteLine(nStr[i]);
+                }
+            }          
+        }
+
+        static void ReadFile(string filename, string[] str)
+        {
+            using (StreamReader reader = File.OpenText(filename))
+            {
+                string line = null;
+                int i = 0;
+                do
+                {
+                   line = reader.ReadLine();
+                   str[i] = line;
+                   i++;
+                }
+                while(line != null);
+            }
+        }
+
+        static int intReadFile(string filename)
+        {
+            int i = 0;
+            using (StreamReader reader = File.OpenText(filename))
+            {
+                string line = null;
+                do
+                {
+                    line = reader.ReadLine();
+                    i++;
+                }
+                while (line != null);
+            }
+            return i;
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("line")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("line")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("b1399d19-ef3d-41b5-9360-0c46a1d982fc")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер построения
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
